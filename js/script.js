b690e443a54ef7a8833d67834ec7416e43ea7eae
John-Phealipto <johnadibe450@gmail.com>
2020-12-06 22:34:51 +0100

    ...

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
 $(document).ready(function(){
-	$(".hamburger").click(function(){
+	$(".hamburger").click(function() {
    	$(this).toggleClass("is-active");
    	$(".panel").slideToggle("slow");
+
    	// $("#mainContent").addClass("d-none");
+   	// var x = $("#mainContent");
+   	// 	if $("#hamburger-11") === $(".is-active") {
+   	// 		x.style.display = "none";
+   	// 	}
+   	// 	else {
+   	// 		x.style.display = "block";
+   	// 	}
   });
 });
 
-
+// function myFunction() {
+//   var x = document.getElementById("mainContent");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
 
 
 // $(document).ready(function(){
