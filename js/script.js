0e02a42d1c0453fa7945c9197aa5ec3f71c1c778
John-Phealipto <johnadibe450@gmail.com>
2020-12-27 16:59:43 +0100

    Resolved the margin for the cards and also the hover function on Our Team page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+   // MENU BUTTON FUNCTION
 	$(".hamburger").click(function() {
    	$(this).toggleClass("is-active");
    	$(".panel").slideToggle("slow");
@@ -7,25 +8,43 @@ $(document).ready(function() {
    });
 
 
+   // CONTACT US
    $(".contact-address-location").hover(function() {
       // $(".fa-map-marker-alt").toggleClass('fa-4x');
    });
-   $(".contact-address-tele").hover(() => {
+   $(".contact-address-tele").hover(function() {
       // $(".fa-address-book").toggleClass("d-none");
    });
 
 
-   $(window).scroll(function(){
-      var scroll = $(window).scrollTop();
-      if (scroll > 0){
-         $("header").addClass('scrolled');
-      } 
-      else{
-         $("header").removeClass('scrolled');
-      }
+   // OUR TEAM
+   $(".main-team-members .card.ceo").hover(function() {
+      $(".ceo .team-member-img").toggleClass('img-border');
+      $(".ceo hr").toggleClass('hr-border');
+      $(".ceo p").toggleClass('text-red');
    });
-   
+   $(".main-team-members .card.cfo").hover(function() {
+      $(".cfo .team-member-img").toggleClass('img-border');
+      $(".cfo hr").toggleClass('hr-border');
+      $(".cfo p").toggleClass('text-red');
+   });
+   $(".main-team-members .card.coo").hover(function() {
+      $(".coo .team-member-img").toggleClass('img-border');
+      $(".coo hr").toggleClass('hr-border');
+      $(".coo p").toggleClass('text-red');
+   });
 
+
+   // $(window).scroll(function(){
+   //    var scroll = $(window).scrollTop();
+   //    if (scroll > 0){
+   //       $("header").addClass('scrolled');
+   //    } 
+   //    else{
+   //       $("header").removeClass('scrolled');
+   //    }
+   // });
+   
 });
 
 
@@ -37,10 +56,3 @@ $(document).ready(function() {
 //     x.style.display = "none";
 //   }
 // }
-
-
-// $(document).ready(function(){
-//   $("#flip").click(function(){
-//     $("#panel").slideToggle("slow");
-//   });
-// });
